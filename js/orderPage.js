e18a98f0903458bb8be68d7cc19421e003e47f5d
郭泽言 <403476891@qq.com>
2018-05-16 13:05:52 +0800

    fix 微信支付

--- js/orderPage.js
+++ js/orderPage.js
@@ -309,7 +309,14 @@ function initPay(){
             });  
             wx.ready(function(res){
 			    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-				
+				wx.checkJsApi({
+				    jsApiList: ['chooseWXPay'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+				    success: function(res) {
+				    		alert(JSON.parse(res));
+				    // 以键值对的形式返回，可用的api值true，不可用为false
+				    // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+				    }
+				});
             });
 		}
 	})
