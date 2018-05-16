c2bc88cbec08d47b0edf80044a293be2a8acf83c
郭泽言 <403476891@qq.com>
2018-05-16 12:58:04 +0800

    fix 微信支付

--- js/orderPage.js
+++ js/orderPage.js
@@ -307,7 +307,7 @@ function initPay(){
                         "chooseWXPay"  
                     ] // 所有要调用的 API 都要加到这个列表中  
             });  
-            wx.error(function(res){
+            wx.ready(function(res){
 			    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
 				
             });
@@ -322,7 +322,7 @@ function pay(data){
         timestamp: data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符  
         nonceStr: data.nonceStr, // 支付签名随机串，不长于 32 位  
         package: data.packageinfo, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）  
-        signType: data.paySign, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'  
+        signType: data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'  
         paySign: data.paySign, // 支付签名  
         success: function(res) { 
         	
@@ -349,3 +349,18 @@ function pay(data){
 }
 
 
+//function onBridgeReady(){
+// WeixinJSBridge.invoke(
+//     'getBrandWCPayRequest', {
+//         "appId":data.appId,     //公众号名称，由商户传入     
+//         "timeStamp":data.timeStamp,         //时间戳，自1970年以来的秒数     
+//         "nonceStr":data.nonceStr, //随机串     
+//         "package":data.packageinfo,     
+//         "signType":data.signType,         //微信签名方式：     
+//         "paySign":data.paySign //微信签名 
+//     },
+//     function(res){     
+//         if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+//     }
+// ); 
+//}s
