18b4d7fa75e05ba13e6561ee79f6ca6900d0526a
郭泽言 <403476891@qq.com>
2018-05-16 13:09:34 +0800

    fix 微信支付

--- js/orderPage.js
+++ js/orderPage.js
@@ -307,23 +307,13 @@ function initPay(){
                         "chooseWXPay"  
                     ] // 所有要调用的 API 都要加到这个列表中  
             });  
-            wx.ready(function(res){
-			    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-				wx.checkJsApi({
-				    jsApiList: ['chooseWXPay'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-				    success: function(res) {
-				    		alert(JSON.parse(res));
-				    // 以键值对的形式返回，可用的api值true，不可用为false
-				    // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-				    }
-				});
-            });
+            
 		}
 	})
 }
 function pay(data){   
   // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-  debugger;
+  alert(JSON.stringify(data));
     wx.chooseWXPay({
         appId: data.appId,  
         timestamp: data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符  
